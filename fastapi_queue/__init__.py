34ca574494a01132d097c42c82637f2097b14fea
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2022-04-23 07:24:58 +0100

    Update

--- fastapi_queue/__init__.py
+++ fastapi_queue/__init__.py
@@ -2,7 +2,7 @@ __author__ = 'WEN (github.com/GoodManWEN)'
 __version__ = ''
 
 from .utils import *
-from .slow_api import RateLimiter
+from .slowapi import RateLimiter
 from .gateway_manager import DistributedTaskApplyManager
 from .worker import QueueWorker
 
